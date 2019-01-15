d01dc43dfaa9f53c8842ae31a55e57b8f503e1a7
Alexander Wingård <alexander.wingard@gmail.com>
2019-01-15 16:37:13 +0100

    tmp

--- test/boc/state_test.clj
+++ test/boc/state_test.clj
@@ -1,10 +1,19 @@
 (ns boc.state-test
   (:require
    [boc.state :as state]
+   [com.rpl.specter :as s]
    [clojure.test :as t :refer [deftest is testing]]
    ))
 
+(defn s-assert [data select expected]
+  (is (= expected (s/select select data)))
+  data)
+
 (deftest sessions
-  (is (= nil (-> {:sessions [{:data {:session "uuid2"}} {:data {:session "uuid4"}}]}
-                 (state/join-session "channel" "uuid")))))
+  (-> {}
+      (state/join-session "channel" "uuid")
+      (s-assert [:sessions s/ALL :channels (s/set-elem "channel")] ["channel"])
+      (state/join-session "channel" "uuid2")
+      (s-assert [:sessions s/ALL (s/selected? [:data :session (s/pred= "uuid")]) :channels (s/set-elem "channel")] []))
+  )
 
