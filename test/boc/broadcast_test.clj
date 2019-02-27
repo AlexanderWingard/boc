417cc5c3e49090db104f22fe7a8617ee3dbbd09b
Alexander Wingård <alexander.wingard@gmail.com>
2019-02-27 23:28:42 +0100

    tmp

--- test/boc/broadcast_test.clj
+++ test/boc/broadcast_test.clj
@@ -7,8 +7,8 @@
 (deftest broadcast-test
   (let [data-old {:sessions [{:data {:session "1" :a 10 :b 10 :c 10} :channels #{1 2}}
                              {:data {:session "2" :a 10 } :channels #{3}}]}
-        data-new {:sessions [{:data {:session "1" :a 20 :c 10} :channels #{1 2}}
+        data-new {:sessions [{:data {:session "1" :a 20 :c 10 :d 20} :channels #{1 2}}
                              {:data {:session "2" :a 10 } :channels #{3}}]}
-        no-debug (->> (be/broadcast-int [data-old data-new] {:a 20} 1)
+        no-debug (->> (be/broadcast-int [data-old data-new] {:a 20 :d 10} 1)
                       (map #(update %1 1 (fn [m] (dissoc m :debug)))))]
-    (is (= '([1 {:b nil}] [2 {:b nil :a 20}] [3 {}]) no-debug))))
+    (is (= '([1 {:b nil :d 20}] [2 {:b nil :a 20 :d 20}] [3 {}]) no-debug))))
