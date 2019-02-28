10df8b03926e19e17ed795e7ec8b68fa9b571b3f
Alexander Wingård <alexander.wingard@gmail.com>
2019-02-28 16:35:38 +0100

    tmp

--- test/boc/sessions_test.clj
+++ test/boc/sessions_test.clj
@@ -3,9 +3,17 @@
    [boc.be.state.sessions :as sessions]
    [clojure.test :refer [deftest is testing]]
    [boc.test-util :refer :all]
+   [com.rpl.specter :as s]
+   [boc.be.state.paths :as paths]
    ))
 
 (deftest sessions
   (-> {}
       (sessions/join 1 "uuid")
-      (s-assert [] nil)))
+      (s-assert [(paths/session "uuid") :channels] #{1})
+      (sessions/join 1 "uuid2")
+      (s-assert [(paths/session "uuid") :channels] #{})
+      (s-assert [(paths/session "uuid2") :channels] #{1})
+      (sessions/leave 1)
+      (s-assert [(paths/session "uuid2") :channels] #{})
+      ))
