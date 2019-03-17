6c933f11544a9056827290344d8776b0d9ea9c32
Alexander Wingård <alexander.wingard@gmail.com>
2019-03-18 00:19:14 +0100

    tmp

--- src/boc/fe/components/main.cljs
+++ src/boc/fe/components/main.cljs
@@ -1,8 +1,9 @@
 (ns boc.fe.components.main
   (:require
+   [boc.fe.components.main-frame :as main-frame]
    [boc.fe.components.general :as gc]
    ))
 
 (defn component []
-  [gc/intent-button :logout "Logout"])
-
+  [main-frame/component
+   "Welcome"])
